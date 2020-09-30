ffd2d57b027be422da522d2ebc9dbff44e27932d
OGharsh <72140506+OGharsh@users.noreply.github.com>
2020-10-01 00:29:08 +0530

    Create my-pull-request.py

---
+++ my-pull-request.py
@@ -0,0 +1,10 @@
+#Code for Hackertoberfest
+
+from tkinter import *
+
+window = tkinter.Tk()
+# to rename the title of the window
+window.title("GUI is meh")
+# pack is used to show the object in the window
+label = tkinter.Label(window, text = "Welcome to guis").pack()
+window.mainloop()
